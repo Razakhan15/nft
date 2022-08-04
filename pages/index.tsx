bc46e5269670453aadaa208e261ac06e0ac2146e
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2022-08-04 23:23:25 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,81 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetServerSideProps, NextPage } from "next";
+import React from "react";
+import Head from "next/head";
+import { sanityClient, urlFor } from "../sanity";
+import { Collection } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[];
+}
+const Home: NextPage = ({ collections }: Props) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+        The{" "}
+        <span className="font-extrabold underline decoration-pink-600/50">
+          NFT
+        </span>{" "}
+        Market Place
+      </h1>
+      <main>
+        <div>
+          {collections.map((collection) => (
+            <div className=" flex flex-col items-center cursor-pointer transition-all duration-200 hover:scale-105">
+              <img
+                src={urlFor(collection.mainImage).url()}
+                alt=""
+                className="h-96 w-60 rounded-2xl object-cover"
+              />
+              <div>
+                <h2 className=" text-3xl">{collection.title}</h2>
+                <p className="mt-2 text-sm text-gray-400">
+                  {collection.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = ` 
+ *[_type == 'collection']{
+  _id,
+  title,
+  address,
+  description,
+  nftCollectionName,
+  mainImage{
+    asset
+  },
+  previewImage{
+    asset
+  },
+  slug{
+    current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug{
+      current
+    },
+  },
+ }`;
+  const collections = await sanityClient.fetch(query);
+  return {
+    props: {
+      collections,
+    },
+  };
+};
